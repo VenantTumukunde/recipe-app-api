612911f794665ad6b8e55f0dbee4d0e93920d1c1
venant <ventum11@gmail.com>
2024-01-10 17:29:56 +0200

    Implement manage user API

--- app/user/views.py
+++ app/user/views.py
@@ -22,7 +22,6 @@ class CreateTokenView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
-
 class ManageUserView(generics.RetrieveUpdateAPIView):
     """Manage the authenticated user."""
     serializer_class = UserSerializer
